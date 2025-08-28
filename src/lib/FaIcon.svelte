a2c3c53c7ef4b3cd07e0ba651bc9a2e3fc0b197a
DivDE <divde@musicociel.fr>
2025-08-28 13:03:19 +0200

    Add Font Awesome

---
+++ src/lib/FaIcon.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import type { IconDefinition } from '@fortawesome/free-solid-svg-icons';
+	import type { SVGAttributes } from 'svelte/elements';
+
+	const { icon, ...rest }: { icon: IconDefinition } & SVGAttributes<SVGElement> = $props();
+
+	const boxWidth = $derived(icon.icon[0]);
+	const boxHeight = $derived(icon.icon[1]);
+	const path = $derived(icon.icon[4] as string);
+</script>
+
+<svg {...rest} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 {boxWidth} {boxHeight}" class={['icon', rest.class]}>
+	<path d={path} />
+</svg>
+
+<style>
+	svg.icon {
+		height: 1em;
+	}
+
+	path {
+		fill: currentColor;
+	}
+</style>
