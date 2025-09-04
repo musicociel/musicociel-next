d11c57ad212ea15507b0bf2804cb6677d6dbe769
DivDE <divde@musicociel.fr>
2025-09-04 12:07:16 +0200

    No longer do a full reload when changing the locale

--- src/routes/[[locale=locale]]/+layout.svelte
+++ src/routes/[[locale=locale]]/+layout.svelte
@@ -25,22 +25,24 @@
 	<link rel="alternate" href={deLocalizeHref(hostAbsolute(page.url))} hreflang="x-default" />
 </svelte:head>
 
-{#if params.locale}
-	<NavBar>
-		<img class="me-2 w-12" src={asset('/favicon.svg')} alt="Logo" />
-		<span class="text-xl">Musicociel</span>
-		<div class="dropdown dropdown-end ms-auto">
-			<button type="button" class="btn m-1"><FaIcon icon={faLanguage} /></button>
-			<ul class="dropdown-content menu z-1 w-52 rounded-box bg-base-100 p-2 shadow-sm">
-				{#each locales as locale (locale)}
-					<li><a data-sveltekit-reload href={localizeHref(hostAbsolute(page.url), { locale })}>{m.language({}, { locale })}</a></li>
-				{/each}
-			</ul>
-		</div>
-	</NavBar>
+{#key params.locale}
+	{#if params.locale}
+		<NavBar>
+			<img class="me-2 w-12" src={asset('/favicon.svg')} alt="Logo" />
+			<span class="text-xl">Musicociel</span>
+			<div class="dropdown dropdown-end ms-auto">
+				<button type="button" class="btn m-1"><FaIcon icon={faLanguage} /></button>
+				<ul class="dropdown-content menu z-1 w-52 rounded-box bg-base-100 p-2 shadow-sm">
+					{#each locales as locale (locale)}
+						<li><a href={localizeHref(hostAbsolute(page.url), { locale })}>{m.language({}, { locale })}</a></li>
+					{/each}
+				</ul>
+			</div>
+		</NavBar>
 
-	<div class="absolute top-0 flex min-h-dvh w-full flex-col">
-		<div class="h-16"></div>
-		{@render children?.()}
-	</div>
-{/if}
+		<div class="absolute top-0 flex min-h-dvh w-full flex-col">
+			<div class="h-16"></div>
+			{@render children?.()}
+		</div>
+	{/if}
+{/key}
